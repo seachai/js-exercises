594de7092473a416583914fc9556a2a34e34ce1d
Chai Lee <42716836+seachai@users.noreply.github.com>
2020-03-08 20:00:57 -0400

    Update palindromes.js

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function() {
+const palindromes = function(word) {
+  let defaultWord = word.replace(/[^A-Z0-9]/gi, "").toLowerCase();
+  let reversedWord = defaultWord
+    .split("")
+    .reverse()
+    .join("");
 
-}
+  if (defaultWord !== reversedWord) {
+    return false;
+  } else return true;
+};
 
 module.exports = palindromes
